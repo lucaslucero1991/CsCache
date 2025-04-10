873d6cc488012b10ddd81d0be438c6360b888b15
lucas.lucero <lucas.lucero@fravega.com.ar>
2025-04-09 23:59:55 -0300

    Se realizan cambios en el dao para agregar algunas funciones

--- Model/Cache_Salas.cs
+++ Model/Cache_Salas.cs
@@ -2,5 +2,9 @@
 
 public class Cache_Salas
 {
-    
+    public short CodSala { get; set; }
+    public string NomSala { get; set; }
+    public int CodTipoSala { get; set; }
+    public string NomTipoSala { get; set; }
+    public int CodComplejo { get; set; }
 }
