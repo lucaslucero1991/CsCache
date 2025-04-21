8cdaf3136b991fcd8a25dbf230432f0bab2ba5ad
llucero <lucaslucero1991@gmail.com>
2025-04-21 10:48:11 -0300

    Use new functions to use cache table

--- Model/Caches.cs
+++ Model/Caches.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CSCache.Model
 {
     public class Caches
     {
         public string IdCache { get; set; }
-        //public Nullable<System.DateTime> UltimaActualizacion { get; set; }
-        public Nullable<System.DateTime> FechaInicio { get; set; }
-        public Nullable<System.DateTime> FechaFin { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
         public string Estado { get; set; }
-        public Nullable<int> Detalle { get; set; }
+        public int? Detalle { get; set; }
         public string Informe { get; set; }
     }
 }
